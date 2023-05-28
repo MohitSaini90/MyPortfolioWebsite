1e57dd505406c1c2b154589256d13393a7dfcdad
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-05-28 20:17:58 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -9,11 +9,3 @@ links.forEach(link => {
     })
 })
 
-//toggle navbar in mobile view
-
-const togglebtn=document.querySelector('.toggle-btn');
-const ul= document.querySelector('.nav-links-container');
-togglebtn.addEventListener('click',() => {
-    togglebtn.classList.toggle('active');
-    ul.classList.toggle('active');
-})
